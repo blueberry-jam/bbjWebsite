0993042f466c5a4996c09fda3214039d59d4172c
GreerPage <greerishere@gmail.com>
2020-03-22 18:42:19 -0400

    set debug to false in settings

--- bbjWebsite/settings.py
+++ bbjWebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pl1d*bw(9usy0)ywi94z2(d7j5ql!b625ce*i!_r9@jn@+^q)r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
